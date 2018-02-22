8b0add48299de2964203b8aca6f39cc6c23e4e90
Luke Lefebure <lefebure@stanford.edu>
2018-02-21 23:00:03 -0800

    save txt versions and add logging

---
+++ cybersecurity_nlp/__init__.py
@@ -0,0 +1,5 @@
+import logging
+
+logger = logging.getLogger(__package__)
+logger.setLevel(logging.INFO)
+logging.basicConfig(format="%(asctime)s:%(levelname)s: %(message)s")
--- cybersecurity_nlp/data/document_accessor.py
+++ cybersecurity_nlp/data/document_accessor.py
@@ -5,6 +5,7 @@ import csv
 import os
 
 from cybersecurity_nlp.data.extract_text import convert_pdf_to_text
+from cybersecurity_nlp import logger
 
 class DocumentAccessor(object):
     """Class for accessing raw documents"""
@@ -15,28 +16,42 @@ class DocumentAccessor(object):
     def _set_paths(self):
         self.key_path = os.path.join(
             os.path.dirname(os.path.realpath(__file__)), "document_key.csv")
-        self.doc_path = os.path.join(
+        self.raw_doc_path = os.path.join(
             os.path.dirname(os.path.realpath(__file__)), "raw")
+        self.text_doc_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "text")
 
     def _parse_file(self):
         rows = []
+        logger.info("Parsing key file")
         with open(self.key_path, newline="") as infile:
             reader = csv.reader(infile)
             Row = namedtuple("Row", next(reader))
             for row in map(Row._make, reader):
                 rows.append(row)
+        logger.info("%d documents found in key file", len(rows))
         self.rows = rows
 
     def documents(self):
         docs = []
         for i, row in enumerate(self.rows):
-            print("Opening: ", os.path.join(self.doc_path, row.File))
+            if i == 2: break
+            text_file = os.path.join(self.text_doc_path, row.File + ".txt")
+            if os.path.isfile(text_file):
+                logger.info("Reading %s from text file", row.File)
+                with open(text_file, "rb") as f:
+                    text = f.read()
+            else:
+                logger.info("Converting %s to text", row.File)
+                text = convert_pdf_to_text(
+                    os.path.join(self.raw_doc_path, row.File))
+                with open(text_file, "w") as f:
+                    f.write(text)
             docs.append({
                 "id": i,
                 "country": row.Country,
                 "year": row.Year,
                 "original_url": row.OriginalURL,
-                "text": convert_pdf_to_text(
-                    os.path.join(self.doc_path, row.File))
+                "text": text
             })
         return docs
