24d111817b80c8785530c0915183b59449353995
Luke Lefebure <lefebure@stanford.edu>
2018-02-21 22:58:30 -0800

    change output path

--- download_documents.sh
+++ download_documents.sh
@@ -1,1 +1,1 @@
-python scripts/download_documents.py --key-file=cybersecurity_nlp/utils/data/document_key.csv --doc-path=cybersecurity_nlp/data/
+python scripts/download_documents.py --key-file=cybersecurity_nlp/data/document_key.csv --doc-path=cybersecurity_nlp/data/raw/
